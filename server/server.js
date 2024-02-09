cfbd47ad8892ca00cbec2fa0ab539a888a5fcd40
Qausimodo75 <kwazikubheka777@gmail.com>
2024-02-09 08:52:31 +0200

    Server is working

--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
+const express = require('express')
 const app = express();
+
+app.use(express.json()); 
+
+
+const PORT = process.env.PORT || 7000;
+
+app.get('/', (req, res) => {
+    res.json({ msg: "The Server is working"});
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is UP on PORT: ${PORT}`);
+  });
+  
