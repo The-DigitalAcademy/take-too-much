e14fc7721ba074d6050198e01065b4c4fb8d0bc7
Qausimodo75 <kwazikubheka777@gmail.com>
2024-02-14 13:41:33 +0200

    registration

--- server/controller/user.js
+++ server/controller/user.js
@@ -4,35 +4,128 @@ const jwt = require('jsonwebtoken');
 
 
 exports.register = async (req, res) => {
+    try {
     const { name, email, role, password } = req.body;
-  
+    
     if (password.length < 6) {
-      return res.status(400).json({ message: "Password less than 6 characters" });
+        return res.status(400).json({ message: "Password less than 6 characters" });
     }
-  
-    const user = await User.findOne({ email: req.body.email });
-  
-    if (user) {
-      res.status(400).json({
-        message: "User already exists",
-      });
-    } else {
-      await User.create({
-        name,
-        email,
-        role,
-        password,
-      }).then((user) =>
-        res.status(200).json({
-          message: "User successfully created",
-          user,
+    if (!email || !password) {
+        return res.status(400).json({
+            message: "email or Password not present",
         })
-      );
     }
-  };  
+    const user = await User.findOne({ email: req.body.email });
+
+   
+        if (user) {
+            res.status(400).json({
+                message: "User already exists",
+            });
+        } else {
+            await User.create({
+                name,
+                email,
+                role,
+                password,
+            }).then((user) =>
+                res.status(200).json({
+                    message: "User successfully created",
+                    user,
+                })
+            );
+        };
+    }
+    catch (error) {
+        res.status(400).json({
+            message: "An error occurred",
+            error: error.message,
+        })
+    }
+}
 
 
-  
+
+
+
+
+
+
+exports.login = async (req, res) => {
+
+    //Req email and password from the user. 
+
+    const { email, password } = req.body
+
+    // Check if username and password is provided
+
+    if (!email || !password) {
+        return res.status(400).json({
+            message: "email or Password not present",
+        })
+    }
+
+    try {
+        const exists = await User.findOne({ email: req.body.email })
+
+        // if(user.email){
+
+        //   return res.status(400).json({
+        //     message: "email or Password not present",
+
+        // },
+
+        // compare the hashed password with the one that the user has inserted// 
+
+        const passwordMatch = await bcrypt.compare(req.body.password, exists.password)
+
+        const user = new User({
+
+            email,
+            password: passwordMatch
+        })
+
+        await user.save()
+
+        if (!passwordMatch) {
+            res.status(401).json({
+
+                message: "Login not successful",
+                error: "User not found",
+            })
+
+        } else {
+
+            const token = jwt.sign(
+                {
+                    _id: user._id,
+                    name: user.name,
+                    email: user.email,
+                    password: user.password,
+                },
+                process.env.JWT_SECRET,
+                { expiresIn: '6h' }
+            )
+
+            res.status(200).json({
+                message: "Login successful",
+                User,
+                token,
+            })
+        }
+    } catch (error) {
+        res.status(400).json({
+            message: "An error occurred",
+            error: error.message,
+        })
+    }
+}
+
+
+
+
+
+
 
 
 
--- server/models/user.js
+++ server/models/user.js
@@ -1,32 +1,34 @@
 const mongoose = require("mongoose");
-const bcrypt = require ("bcrypt")
+const bcrypt = require("bcrypt")
 const userSchema = new mongoose.Schema(
   {
+
+    name: {
+      type: String,
+      required: true
+    },
     email: {
       type: String,
       required: true,
-      unique: true,
+      unique: true
     },
-    name:{
-      type: String,  
-     
-    }, 
-    password:{ 
-    type: String, 
-    required: true, 
-    }, 
-    role:{
-        type: String,
-        required: true,
-      },
+
+    password: {
+      type: String,
+      required: true
     },
-    { timestamps: true }
-  ); 
-  // this allow the user password to be hashed// 
-  userSchema.pre("save", async function (next) {
-    const salt = await bcrypt.genSalt();
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
+    role: {
+      type: String,
+      required: true
+    },
+  },
+  { timestamps: true }
+);
+// this allow the user password to be hashed// 
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 
 });
 module.exports = mongoose.model("Users", userSchema);
