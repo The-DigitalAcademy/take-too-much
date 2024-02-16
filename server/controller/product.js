0e8f9841cc030f41e9e372b193a0b643b6b3e7a5
Qausimodo75 <kwazikubheka777@gmail.com>
2024-02-16 11:47:32 +0200

    viewing all products and login is working

--- server/controller/product.js
+++ server/controller/product.js
@@ -1,27 +1,27 @@
-const  Item = require("../models/products")
+const Item = require("../models/products")
 
 
 // Create a specific product for the store
 
 exports.create = async (req, res) => {
-    try {
-      const newItem = new Item(req.body);
-      await newItem.save();
-      res.status(201).json({ message: "The item has been created" });
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
+  try {
+    const newItem = new Item(req.body);
+    await newItem.save();
+    res.status(201).json({ message: "The item has been created" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
+
+
+
+exports.read = async (req, res) => {
+  try {
+    const product = await Item.find();
+    res.json(product);
+  } catch (error) {
+    res.json({ message: "Something went wrong, try again later" });
   }
 
 
-
-  exports.read = async (req, res) => {
-    try {
-      const product = await Item.find();
-       res.json(product);
-    } catch (error) {
-       res.json({ message: "Something went wrong, try again later" });
-     }
-
-
-  }
+}
